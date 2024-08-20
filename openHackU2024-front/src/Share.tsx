9c8543378a9e783170d5f5d9a7b9426e68a62ba2
Gomamono <k22057kk@aitech.ac.jp>
2024-08-21 07:44:48 +0900

    とりまShare作成

---
+++ openHackU2024-front/src/Share.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Share: React.FC = () => {
+    return (
+        <div>
+            <h1>Share</h1>
+            <p>共有中の画面だよ</p>
+            <p>ルーム</p>
+        </div>
+    );
+};
+
+export default Share;
