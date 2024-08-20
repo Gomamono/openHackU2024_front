d507ff749047e5cf2fe8a5c2b0ff65d77c0f6ff9
Gomamono <k22057kk@aitech.ac.jp>
2024-08-21 00:49:31 +0900

    menubarで画面遷移できる(たぶん)

--- openHackU2024-front/src/components/menuBar.tsx
+++ openHackU2024-front/src/components/menuBar.tsx
@@ -1,16 +1,16 @@
-// src/components/MenuBar.tsx
-
 import React from 'react';
 import { FaHome, FaCog } from 'react-icons/fa';
-//import './MenuBar.css';
+import { useNavigate } from 'react-router-dom';
 
 const MenuBar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="menu-bar">
-      <div className="menu-item">
+      <div className="menu-item" onClick={() => navigate('/')}>
         <FaHome size={32} />
       </div>
-      <div className="menu-item">
+      <div className="menu-item" onClick={() => navigate('/settings')}>
         <FaCog size={32} />
       </div>
     </div>
