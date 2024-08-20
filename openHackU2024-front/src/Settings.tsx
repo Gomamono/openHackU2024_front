f1707ceed0cf0fc9d1d48f6ac290f65feb62c63e
Gomamono <k22057kk@aitech.ac.jp>
2024-08-21 01:35:11 +0900

    settings仮作成1

---
+++ openHackU2024-front/src/Settings.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MenuBar from './components/MenuBar';
+
+const Settings: React.FC = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>ここでユーザーの設定を変更することができます。</p>
+      {/* 設定項目を追加してください */}
+        <MenuBar />
+    </div>
+  );
+};
+
+export default Settings;
